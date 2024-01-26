9e8ab37ac97d0fa8126665c0bd2732b11fcd8f7f
d-nieuwenhuijzen <302134932@student.rocmondriaan.nl>
2024-01-26 10:28:47 +0100

    p03 les 1

--- week 1 p03/js/main.js
+++ week 1 p03/js/main.js
@@ -1,23 +1,24 @@
-// const pokemons = [
-//     {pokemon: "squirtle", descripition: "ja"},
-//     {pokemon: "scorbunny", description: "ja"},
-// ]
-
-// function createCard(pokemon, description){
-//     let cardHTML =
-
-//     `<div class="card card-1">
-//         <h2>card 1</h2>
-//         <p>Lorem Ipsum</p>
-//         <button>button 1</button>
-//     </div>`;
-// }
-
-
-const projects = [
-    {
-    title: "Hoger Lager",
-     description: "dit was mijn eerste school project",
-      image: "img/jush.webp"
-    },
+// wat ik mee heb gekregen van het card js stukje
+const pokemons = [
+    {pokemon: "squirtle", descripition: "ja"},
+    {pokemon: "scorbunny", description: "ja"},
 ]
+
+function createCard(pokemon, description){
+    let cardHTML =
+
+    `<div class="card card-1">
+        <h2>card 1</h2>
+        <p>Lorem Ipsum</p>
+        <button>button 1</button>
+    </div>`;
+}
+
+// code voor POFO
+// const projects = [
+//     {
+//     title: "Hoger Lager",
+//      description: "dit was mijn eerste school project",
+//       image: "img/jush.webp"
+//     },
+// ]
